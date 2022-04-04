15d573f5e52cf4e468725b2320c09354fc91014a
группы стационара <st@itstep.lan>
2022-04-04 21:30:01 +0300

    Initial commit

---
+++ src/test/java/selenium/FirstTest.java
@@ -0,0 +1,40 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class FirstTest {
+
+    @Test
+    public void firstTest() throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "D:/Automated Testing/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+        driver.get("https://www.onliner.by/");
+        Thread.sleep(3000);
+
+//        WebElement element = driver.findElement(By.name("query"));
+
+//        element.sendKeys("вентилятор");
+//        Thread.sleep(3000);
+//        element.submit();
+
+        Thread.sleep(3000);
+        WebElement vent = driver.findElement(By.xpath("//h2/a[contains(.,'Кошелек')]"));
+        vent.click();
+
+        System.out.println(driver.getTitle());
+//        driver.quit();
+    }
+
+}
