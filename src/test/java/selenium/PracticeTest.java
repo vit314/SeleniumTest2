acd20db7ce51c25d078819207a0e123791ea3bd9
vit314 <paulahradski@gmail.com>
2022-04-13 22:00:21 +0300

    changes

--- src/test/java/selenium/PracticeTest.java
+++ src/test/java/selenium/PracticeTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -23,23 +24,30 @@ public class PracticeTest {
         driver.get("https://selectorshub.com/xpath-practice-page/");
         Thread.sleep(3000);
 
-        WebElement userEmail = driver.findElement(By.id("userId"));
-        userEmail.sendKeys("test@test.com");
-        Thread.sleep(3000);
+        String titleActual = driver.getTitle();
+        String titleExpected = "XPath & cssSelector Practice Page with all scenarios – SelectorsHub";
 
-        WebElement password = driver.findElement(By.id("pass"));
-        password.sendKeys("pass123");
-        Thread.sleep(3000);
+        Assert.assertEquals(titleActual, titleExpected);
 
-        WebElement company = driver.findElement(By.name("company"));
-        company.sendKeys("qa company");
-        Thread.sleep(3000);
 
-        WebElement submit = driver.findElement(By.xpath("//input[@type='submit']"));
-        submit.click();
-        Thread.sleep(3000);
+//
+//        WebElement userEmail = driver.findElement(By.id("userId"));
+//        userEmail.sendKeys("test@test.com");
+//        Thread.sleep(3000);
+//
+//        WebElement password = driver.findElement(By.id("pass"));
+//        password.sendKeys("pass123");
+//        Thread.sleep(3000);
+//
+//        WebElement company = driver.findElement(By.name("company"));
+//        company.sendKeys("qa company");
+//        Thread.sleep(3000);
+//
+//        WebElement submit = driver.findElement(By.xpath("//input[@type='submit']"));
+//        submit.click();
+//        Thread.sleep(3000);
 
-        driver.quit();
+//        driver.quit();
 
     }
 }
---
+++ src/test/java/selenium/SauceDemoNegativeTest.java
@@ -0,0 +1,64 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class SauceDemoNegativeTest {
+
+    @Test
+    public void SauceDemoNegativeTest() throws InterruptedException {
+
+        // СТАНДАРТНАЯ ДЛЯ ВСЕХ ТЕСТОВ ЧАСТЬ
+        System.setProperty("webdriver.chrome.driver", "D:/Automated Testing/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        // СТАНДАРТНАЯ ЧАСТЬ
+
+        // НАЧАЛО ТЕСТА
+        driver.get("https://www.saucedemo.com/");
+        Thread.sleep(3000);
+
+        //ПРОВЕРКА ОТКРЫЛАСЬ НУЖНАЯ СТРАНИЦА
+        String titleActual = driver.getTitle();
+        String titleExpected = "Swag Labs";
+
+        Assert.assertEquals(titleActual, titleExpected);
+        System.out.println("Заголовок страницы проверен");
+
+        // ВВОД ЛОГИН И ПАРОЛЬ
+        WebElement userName = driver.findElement(By.id("user-name"));
+        userName.sendKeys("test@test.com");
+        Thread.sleep(1000);
+
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("pass123");
+        Thread.sleep(1000);
+
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        loginButton.click();
+        Thread.sleep(3000);
+
+        //ИЗВЛЕЧЕНИЕ ТЕКСТА ИЗ ЭЛЕМЕНТА НА СТРАНИЦЕ
+        WebElement errorMessage = driver.findElement(By.xpath("//h3[@data-test='error']"));
+        String actualTextMessage = errorMessage.getText();
+
+        String expectedMessage = "Epic sadface: Username and password do not " +
+                "match any user in this service";
+
+        // ПРОВЕРКА ТЕКСТА СООБЩЕНИЯ
+        Assert.assertEquals(actualTextMessage, expectedMessage);
+        System.out.println("Текст сообщения об ошибке проверен");
+
+        driver.quit();
+
+    }
+}
